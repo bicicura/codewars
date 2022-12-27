a2ba1696a2af21add1531452018629638140419a
FrancoLM <fr4ncolamattina@gmail.com>
2022-12-27 20:28:18 -0300

    list filtering | javascript

---
+++ PersistentBugger.js
@@ -0,0 +1,21 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+const numToArray = num => num.toString().split('').map(value => +value)
+const multiplyArray = arr => arr.reduce((a, b) => a * b)
+
+function persistence(num) {
+  if (num < 9) return 0
+  let arrayReduced = multiplyArray(numToArray(num))
+  let steps = 1
+  while(arrayReduced > 9) {
+    steps = steps + 1
+    arrayReduced = multiplyArray(numToArray(arrayReduced))
+  }
+  return steps
+}
